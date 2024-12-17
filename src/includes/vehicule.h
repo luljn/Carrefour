bd184ab526596c4dc100ddb2074310d3706e0347
luljn <mbohlulajonathan4@gmail.com>
2024-12-17 17:09:09 +0100

    Structure Vehicule upgraded with a pointer to construct a file

--- src/includes/vehicule.h
+++ src/includes/vehicule.h
@@ -10,6 +10,7 @@ struct Vehicule
     char type[100];          // Type du véhicule. (voiture, ambulance, etc...)
     float vitesse;          // Vitesse du véhicule en km/h.
     char destination[100]; // Destination du véhicule.
+    Vehicule *suivant;    // Le véhicule suivant (pour la structure des files d'attente).
 };
 
 Vehicule initialiserVehicule(int id, char type[100], float vitesse, char destination[100]);

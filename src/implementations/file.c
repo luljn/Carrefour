f7b6eb5bfd1635c508c99e466a2aa0bb25a72b0b
luljn <mbohlulajonathan4@gmail.com>
2024-12-17 17:08:05 +0100

    Structure File added

---
+++ src/implementations/file.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../includes/file.h"
+
+
+
+File* initialisation(){
+
+}
+
+void ajouter(File* file, Vehicule vehicule){
+
+}
+
+void supprimer(File* file){
+    
+}
---
+++ src/includes/file.h
@@ -0,0 +1,18 @@
+#ifndef DEF_FILE
+#define DEF_FILE
+#include "vehicule.h"
+
+
+typedef struct File File;
+struct File
+{
+    Vehicule *premier;
+};
+
+File* initialisation();
+void ajouter(File* file, Vehicule vehicule);
+void supprimer(File* file);
+
+
+
+#endif

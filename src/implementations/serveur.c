dc4b68c1d261d0ea7f7c981cb368830dfdf1671f
luljn <mbohlulajonathan4@gmail.com>
2024-12-24 23:27:50 +0100

    Struct 'Message' added

--- src/implementations/serveur.c
+++ src/implementations/serveur.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 #include "../includes/file.h"
 #include "../includes/serveur.h"
 
--- src/implementations/vehicule.c
+++ src/implementations/vehicule.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 #include "../includes/vehicule.h"
 
 
